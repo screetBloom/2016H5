1160b57c848be8eaa5c6ddcf5b5e3f91f777b104
wei_chen <707885370@qq.com>
2017-07-01 13:56:18 +0800

    Update chen_util.js

--- util/chen_util.js
+++ util/chen_util.js
@@ -260,3 +260,32 @@ function chen_ajaxPost(url,data,callback) {
    }
 
 
+//快速排序
+function chen_quickSort(array){
+    function sort(prev, numsize){
+        var nonius = prev;
+        var j = numsize -1;
+        var flag = array[prev];
+        if ((numsize - prev) > 1) {
+            while(nonius < j){
+                for(; nonius < j; j--){
+                    if (array[j] < flag) {
+                        array[nonius++] = array[j];　//a[i] = a[j]; i += 1;
+                        break;
+                    };
+                }
+                for( ; nonius < j; nonius++){
+                    if (array[nonius] > flag){
+                        array[j--] = array[nonius];
+                        break;
+                    }
+                }
+            }
+            array[nonius] = flag;
+            sort(0, nonius);
+            sort(nonius + 1, numsize);
+        }
+    }
+    sort(0, array.length);
+    return array;
+}
